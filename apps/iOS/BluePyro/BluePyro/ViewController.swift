4d2c5d2836116c049d1284e9e0686781728dd5bc
homanhtai <homanhtai@gmail.com>
2021-09-17 16:38:09 -0400

    Tai updated: Modify name

---
+++ apps/iOS/BluePyro/BluePyro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluePyro
+//
+//  Created by Tai on 2021-08-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
